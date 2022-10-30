b6cfcbc45f7c8868c33305137b9379ddeb5547d1
THEBIGMAC\andrew <andrew_mcalister@hotmail.com>
2022-10-31 00:28:40 +1100

    React App, Nearly works: Request sucessful, but not displaying answer

--- weatherui/src/App.js
+++ weatherui/src/App.js
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import WeatherForm from "./weatherform";
 import "./App.css";
-import axios from "axios";
-
-//'http://localhost:8081/api/v1.0/Weather?country=uk&city=london'
 
 const App = () => {
   return (
--- weatherui/src/weatherform.js
+++ weatherui/src/weatherform.js
@@ -1,37 +1,60 @@
 import React from "react";
-import { useState } from "react";
-import { useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const WeatherForm = () => {
-  let countryInput = React.createRef();
-  let cityInput = React.createRef();
-  let messageRef = React.createRef();
+  const [state, setState] = useState({
+    country: "",
+    city: "",
+    message: "No Data Yet",
+  });
 
-  const [message, setMessage] = useState("No Weather Yet");
+  useEffect(() => {
+    const data = window.localStorage.getItem("WEATHER_STATE");
+    if (data != null) setState(JSON.parse(data));
+  }, []);
 
-  const onSearchSubmit = async (country, city) => {
-    console.log(country + " " + city);
-    const response = axios.get("http://localhost:8081/api/v1.0/weather", {
-      params: { country, city },
-      headers: {
-        Accept: "text/plain",
-      },
+  useEffect(() => {
+    window.localStorage.setItem("WEATHER_STATE", JSON.stringify(state));
+  }, [state]);
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
     });
-    console.log(await response.data.result.data);
-    setMessage(response.data.result);
-    messageRef.current.InnerHTML = message;
+  };
+
+  const onSearchSubmit = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8081/api/v1.0/weather",
+        {
+          params: { country: state.country, city: state.city },
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      setState({ ...state, message: response.data.data });
+      window.localStorage.setItem("WEATHER_STATE", JSON.stringify(state));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <form>
-      <h1 className="Header">Weather</h1>
+      <h1 className="page-heading">What Is The Weather Doing?</h1>
+      <br />
       <h2>
         <input
           type="text"
           placeholder="Enter Country Name"
           required
-          ref={countryInput}
+          value={state.country}
+          onChange={handleChange}
+          name="country"
         />
       </h2>
       <h2>
@@ -39,19 +62,20 @@ const WeatherForm = () => {
           type="text"
           placeholder="Enter City Name"
           required
-          ref={cityInput}
+          value={state.city}
+          onChange={handleChange}
+          name="city"
         />
       </h2>
-      <h2 ref={messageRef}>{message}</h2>
+      <br />
       <span>
-        <button
-          onClick={() =>
-            onSearchSubmit(countryInput.current.value, cityInput.current.value)
-          }
-        >
-          Go!
-        </button>
+        <button onClick={() => onSearchSubmit()}>Find Out</button>
       </span>
+      <br />
+      <br />
+      <h2>
+        <label name="message">{state.message}</label>
+      </h2>
     </form>
   );
 };
